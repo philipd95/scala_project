255017ab4da8e83af2e222741c146314cedce69c
kronen77 <even__kj@hotmail.com>
2019-10-23 13:42:01 +0200

    Documentation Account

--- scala_project/project_tasks/src/main/scala/Account.scala
+++ scala_project/project_tasks/src/main/scala/Account.scala
@@ -6,25 +6,40 @@ class Account(val bank: Bank, initialBalance: Double) {
 
     val balance = new Balance(initialBalance)
 
-    // TODO
-    // for project task 1.2: implement functions
-    // for project task 1.3: change return type and update function bodies
+    /*
+    Function that withdraws an amount from the account
+    Takes an amount of type double
+    Returns Either[Double, String]
+    */
     def withdraw(amount: Double): Either[Double, String] =   this.synchronized {
         if (amount < 0) return Right("This is not a positive number")
         else if (amount > balance.amount) return Right("Funds not available")
         balance.amount -= amount
         Left(amount)
     }
+    /*
+    Function that deposits an amount from the account
+    Takes an amount of type double
+    Returns Either[Double, String]
+    */
     def deposit (amount: Double): Either[Double, String] = this.synchronized {
         if (amount < 0) return Right("This is not a positive number")
         balance.amount += amount
         Left(amount)
 
     }
+    /*
+    Gets the balance in the account
+    Returns a Double
+    */
     def getBalanceAmount: Double = this.synchronized {
         balance.amount
     }
 
+    /*
+    Adds a transaction the the transaction queue
+    Takes an account of type Account and an amount of type Double
+    */
     def transferTo(account: Account, amount: Double) = {
         bank addTransactionToQueue (this, account, amount)
     }
